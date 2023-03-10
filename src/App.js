8379314c6db1f67c1858f126914d1f662b65e470
Duong Tan Binh <v.binhdt26@vinbigdata.com>
2023-03-10 16:11:52 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Landing from "pages/Landing"
+import Login from "pages/Login"
+import Register from "pages/Register"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route element={<Landing />} path="/" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<Register />} path="/register" />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Landing/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>lading</div>
+  )
+}
+
+export default Landing
